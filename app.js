70f8ff9aced0173bf09655e1a441bc0e9c481384
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-29 00:14:28 +1100

    add comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let json = require('./results.json');
+let json = require('./results.json');       // TODO: use github api directly
 const execSync = require('child_process').execSync;
 
 let items = json.items;
@@ -8,7 +8,7 @@ for (let i = 0; i < items.length; i++) {
     let fullName = items[i].repository.full_name;
     let cloneLink = `git@github.com:${fullName}.git`;
 
-    // cd into clones folder and clone repo
+    // cd into clones folder and clone repo     FIXME: create clones folder if doesn't exist
     let cmd = `cd ./clones && git clone ${cloneLink}`;
 
     // add name of repo to files.txt
