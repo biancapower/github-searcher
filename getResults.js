f3f7273fd3a2458fc5c64c77f1ff092186c2d70a
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-29 09:48:57 +1100

    fetch results from github api

---
+++ getResults.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const fetch = require('node-fetch');
+const btoa = require('btoa');
+global.Headers = fetch.Headers;
+
+let url = 'https://api.github.com/search/code?q=setExpandEntityReferences+in:file+language:java+sort:interactions&per_page=3';
+
+let username = process.env.GITHUB_USERNAME;
+let password = process.env.GITHUB_PASSWORD;
+
+let authString = `${username}:${password}`
+let headers = new Headers();
+headers.set('Authorization', 'Basic ' + btoa(authString))
+
+fetch(url, { headers: headers }).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    console.log(data);
+}).catch(function () {
+    console.log("Err");
+});
