0bb48d1d71c810c0d9c9b57be2d34efc59bb1328
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-31 09:14:59 +1100

    add comment

--- getResults.js
+++ getResults.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 const btoa = require('btoa');
 global.Headers = fetch.Headers;
 
+// max per_page=100, to loop through pages use &page=${i}
 let url = 'https://api.github.com/search/code?q=setExpandEntityReferences+in:file+language:java+sort:interactions&per_page=3';
 
 let username = process.env.GITHUB_USERNAME;
