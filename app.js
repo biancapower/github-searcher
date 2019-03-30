d192efac4cbdf5e38d02857b8fa345e6811354f8
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-31 09:13:21 +1100

    add formatting to files.txt output

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ for (let i = 0; i < items.length; i++) {
     let name = items[i].repository.name;
     let fullName = items[i].repository.full_name;
     let cloneLink = `git@github.com:${fullName}.git`;
+    let htmlLink = items[i].repository.html_url;
 
     // cd into clones folder and clone repo     FIXME: create clones folder if doesn't exist
     let cmd = `cd ./clones && git clone ${cloneLink}`;
 
-    // add name of repo to files.txt
-    cmd += ` && echo ${name} >> ./../files.txt`
+    // add repo name and link to files.txt
+    cmd += ` && echo '\n============\n' ${name.toUpperCase()}    '('${htmlLink}')' '\n============' >> ./../files.txt`
 
-    // cd into folder, run script to check for desired results, append names of files to files.txt, cd out of folder
-    cmd += ` && cd ${name} && ./../../vuln-script.sh >> ./../../files.txt && cd ./../../`;
+    // cd into folder, run script to check for desired results, append names of files to files.txt, cd out of folder (sed adds formatting)
+    cmd += ` && cd ${name} && ./../../vuln-script.sh | sed 's/^/--- /' >> ./../../files.txt && cd ./../../`;
 
     // remove repo      FIXME: only do this if doesn't contain desired results
     // cmd += `&& rm -rf ${name}`;
