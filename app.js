723d330ee230be64c9cbb6ac99737adfc32afad7
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-03-31 11:08:16 +1100

    get results directly from api

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-let json = require('./results.json');       // TODO: use github api directly
+// let json = require('./results.json');       // TODO: use github api directly
+const githubApiCall = require('./getResults');
 const execSync = require('child_process').execSync;
 
-let items = json.items;
+githubApiCall
+.then(function (json) {
 
-for (let i = 0; i < items.length; i++) {
-    let name = items[i].repository.name;
-    let fullName = items[i].repository.full_name;
-    let cloneLink = `git@github.com:${fullName}.git`;
-    let htmlLink = items[i].repository.html_url;
+    let items = json.items;
 
-    // cd into clones folder and clone repo     FIXME: create clones folder if doesn't exist
-    let cmd = `cd ./clones && git clone ${cloneLink}`;
+    for (let i = 0; i < items.length; i++) {
+        let name = items[i].repository.name;
+        let fullName = items[i].repository.full_name;
+        let cloneLink = `git@github.com:${fullName}.git`;
+        let htmlLink = items[i].repository.html_url;
 
-    // add repo name and link to files.txt
-    cmd += ` && echo '\n============\n' ${name.toUpperCase()}    '('${htmlLink}')' '\n============' >> ./../files.txt`
+        // cd into clones folder and clone repo     FIXME: create clones folder if doesn't exist
+        let cmd = `cd ./clones && git clone ${cloneLink}`;
 
-    // cd into folder, run script to check for desired results, append names of files to files.txt, cd out of folder (sed adds formatting)
-    cmd += ` && cd ${name} && ./../../vuln-script.sh | sed 's/^/--- /' >> ./../../files.txt && cd ./../../`;
+        // add repo name and link to files.txt
+        cmd += ` && echo '\n============\n' ${name.toUpperCase()}    '('${htmlLink}')' '\n============' >> ./../files.txt`
 
-    // remove repo      FIXME: only do this if doesn't contain desired results
-    // cmd += `&& rm -rf ${name}`;
+        // cd into folder, run script to check for desired results, append names of files to files.txt, cd out of folder (sed adds formatting)
+        cmd += ` && cd ${name} && ./../../vuln-script.sh | sed 's/^/--- /' >> ./../../files.txt && cd ./../../`;
 
-    const output = execSync(cmd, { encoding: 'utf-8' });
-}
+        // remove repo      FIXME: only do this if doesn't contain desired results
+        // cmd += `&& rm -rf ${name}`;
 
+        const output = execSync(cmd, { encoding: 'utf-8' });
+    }
+    
+}).catch(function () {
+    console.error("Err");
+});
 
--- getResults.js
+++ getResults.js
@@ -4,7 +4,7 @@ const btoa = require('btoa');
 global.Headers = fetch.Headers;
 
 // max per_page=100, to loop through pages use &page=${i}
-let url = 'https://api.github.com/search/code?q=setExpandEntityReferences+in:file+language:java+sort:interactions&per_page=3';
+let url = 'https://api.github.com/search/code?q=setExpandEntityReferences+in:file+language:java+sort:interactions&page=4&per_page=3';
 
 let username = process.env.GITHUB_USERNAME;
 let password = process.env.GITHUB_PASSWORD;
@@ -13,10 +13,6 @@ let authString = `${username}:${password}`
 let headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(authString))
 
-fetch(url, { headers: headers }).then(function (response) {
+module.exports=fetch(url, { headers: headers }).then(function (response) {
     return response.json();
-}).then(function (data) {
-    console.log(data);
-}).catch(function () {
-    console.log("Err");
 });
